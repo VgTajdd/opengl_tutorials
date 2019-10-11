9f1f6a96e545dcbe3a39d93e66f78444dc662365
VgTajdd <aduranddiaz@gmail.com>
2019-10-11 12:08:59 -0500

    Fix window title according to tutorials

--- ep_14_buffer_layout_abstraction/src/main.cpp
+++ ep_14_buffer_layout_abstraction/src/main.cpp
@@ -128,7 +128,7 @@ int main( void )
 	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
 	// Open a window and create its OpenGL context.
-	GLFWwindow* window = glfwCreateWindow( 1024, 768, "OpenGL Tutorial - Vertex arrays", NULL, NULL );
+	GLFWwindow* window = glfwCreateWindow( 1024, 768, "OpenGL Tutorial - Buffer layout abstraction", NULL, NULL );
 	if ( window == NULL )
 	{
 		/*fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
